46d74934106e5712dfa184c668acb258ab0d076f
Daniel Yoo <daniel.yoo0523@gmail.com>
2021-03-26 17:13:54 -0400

    compelted game handler class, need to process inputs now

--- GameHandler.py
+++ GameHandler.py
@@ -1,7 +1,102 @@
-import SnakeGame from SnakeGame
+from SnakeGame import SnakeGame 
+import constants
+import pygame, time
 
 class GameHandler:
     #abstraction to handle inputs
 
+    _game         = None
+    _width        = None
+    _height       = None
+    _pi           = None
+    _pixels       = None
+    _FPSCLOCK     = None
+    _DISPLAYSURF  = None
+    _BASICFONT    = None
+    _BIGFONT      = None
 
-    def __init__():
+    def __init__(self, width, height, pi):
+        self._game = SnakeGame(width, height, pi)
+        self._pi = pi
+        self._height = height
+        self._width = width
+        if pi:
+            import neopixel, board
+            pixel_pin = board.D18
+            num_pixels = width * height
+            order = neopixel.GRB
+            self._pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=constants.LED_BRIGHTNESS, auto_write=False,pixel_order=order)
+            pygame.init()
+            #comment out once controller connected
+            pygame.display.set_mode((width*constants.SIZE, width*constants.SIZE))
+        else:
+            pygame.init()
+            self._FPSCLOCK = pygame.time.Clock()
+            self._DISPLAYSURF = pygame.display.set_mode((width*constants.SIZE, height*constants.SIZE))
+            self._BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+            self._BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
+            pygame.display.set_caption('Pi Games')
+            self._DISPLAYSURF.fill(BGCOLOR)
+            pygame.display.update()
+        
+    def loop(self):
+        try:
+            while True:
+                time.sleep(1)
+                self._game.proceed()
+                self.update()
+        except KeyboardInterrupt:
+            self.clear()
+            self.updateScreen()
+
+
+    def drawPixel(self, x, y, color):
+        if self._pi:
+            try:
+                if (x>=0 and y>=0 and color >=0):
+                    self._pixels[self.getPixelFromGrid(x,y)] = constants.COLORS[color]
+            except:
+                print(str(x) + ' --- ' + str(y))   
+        else:
+            pygame.draw.rect(DISPLAYSURF, constants.COLORS[color], (x*constants.SIZE+1, y*constants.SIZE+1, constants.SIZE-2, constants.SIZE-2))
+
+    #helper to translate the continuous strip into a grid
+    def getPixelFromGrid(self, x, y):
+        if y%2 == 0:
+            x = self._width - 1 - x
+        return self._width * y + x
+
+    #blank the screen
+    def clear(self):
+        if self._pi:
+            self._pixels.fill((0,0,0))
+        else:
+            self._DISPLAYSURF.fill(BGCOLOR)
+
+    #draw the snake
+    def drawSnake(self):
+        snake = self._game.getSnake()
+        snakeHead = snake.getHead()
+        snakeNodes = snake.getNodes()
+        for snakeNode in snakeNodes[1:]:
+            self.drawPixel(snakeNode['x'], snakeNode['y'], 1)
+            self.drawPixel(snakeHead['x'], snakeHead['y'], 2)
+
+    #draw the apple
+    def drawApple(self):
+        apple = self._game.getApple()
+        self.drawPixel(apple['x'], apple['y'], 3)
+
+    #update the leds/pixels
+    def updateScreen(self):
+        if self._pi:
+            self._pixels.show()
+        else:
+            pygame.display.update()
+
+    #main update that gets called during the loop to update the screen
+    def update(self):
+        self.clear()
+        self.drawSnake()
+        self.drawApple()
+        self.updateScreen()
---
+++ constants.py
@@ -0,0 +1,45 @@
+LED_FREQ_HZ     = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA         = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS  = 100     # Set to 0 for darkest and 255 for brightest
+LED_INVERT      = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL     = 0       # set to '1' for GPIOs 13, 19, 41, 45 or
+
+#CONSTANTS
+BLANK = '.'
+#               R    G    B
+WHITE       = (255, 255, 255)
+GRAY        = (185, 185, 185)
+BLACK       = (  0,   0,   0)
+RED         = (255,   0,   0)
+LIGHTRED    = (175,  20,  20)
+GREEN       = (  0, 255,   0)
+LIGHTGREEN  = ( 20, 175,  20)
+BLUE        = (  0,   0, 255)
+LIGHTBLUE   = ( 20,  20, 175)
+YELLOW      = (255, 255,   0)
+LIGHTYELLOW = (175, 175,  20)
+CYAN        = (  0, 255, 255)
+MAGENTA     = (255,   0, 255)
+ORANGE      = (255, 100,   0)
+
+COLORS      = (BLUE,GREEN,RED,YELLOW,CYAN,MAGENTA,ORANGE)
+
+WIDTH       = 10
+HEIGHT      = 20
+ 
+KEY_MAPPING = {
+    "274": "down",
+    "115": "down",
+    "273": "up",
+    "119": "up",
+    "276": "left",
+    "97": "left",
+    "275": "right",
+    "100": "right"
+}
+
+BORDERCOLOR = BLUE
+BGCOLOR = BLACK
+TEXTCOLOR = WHITE
+TEXTSHADOWCOLOR = GRAY
+SIZE = 20
